f0f1d37a90aa9e85ce7f1fa8893483b6dcde0170
Kavinthe Perera <kavintheperera@gmail.com>
2024-06-08 22:35:24 +0530

    settings modal UI + settings for searchbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
 import "./App.css";
+import { FiSettings } from "react-icons/fi";
 import Searchbar from "./components/searchbar";
+import ChangeSettings from "./components/settingsModal";
 
-function App() {
+export default function App() {
   const [settings, setSettings] = useState(() => {
     const localValue = localStorage.getItem("settings");
     if (localValue == null) {
       const defaultSettings = {
+        searchBar: true,
         searchEngine: "google",
-        background: "dark",
       };
       localStorage.setItem("settings", JSON.stringify(defaultSettings));
       return defaultSettings;
@@ -17,10 +19,29 @@ function App() {
   });
 
   return (
-    <>
-      <Searchbar searchEngine={settings.searchEngine} />
-    </>
+    <div
+      style={{
+        backgroundColor: "#120312",
+        backgroundImage:
+          "linear-gradient(296deg, #120312 1%, #020e18 50%, #0f0414 97%)",
+      }}
+      className="antialiased overflow-hidden"
+    >
+      <button
+        className="fixed top-0 right-0 z-50 m-4"
+        onClick={() => document.getElementById("SettingsModal").showModal()}
+      >
+        <FiSettings />
+      </button>
+      <div className="flex justify-center items-center h-screen ">
+        <div>
+          {settings.searchBar && (
+            <Searchbar searchEngine={settings.searchEngine} />
+          )}
+          <ChangeSettings setSettings={setSettings} settings={settings} />
+        </div>
+      </div>
+    </div>
   );
 }
 
-export default App;
---
+++ src/components/settingsModal/index.tsx
@@ -0,0 +1,61 @@
+export default function ChangeSettings({
+  setSettings,
+  settings,
+}: {
+  setSettings: [] | any;
+  settings: [] | any;
+}) {
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const newSettings = {
+      searchBar: formData.get("searchBar") === "on" ? true : false,
+      searchEngine: formData.get("searchEngine") as string,
+      background: formData.get("background") as string,
+    };
+    setSettings(newSettings);
+    localStorage.setItem("settings", JSON.stringify(newSettings));
+  };
+
+  return (
+    <dialog id="SettingsModal" className="modal">
+      <div className="modal-box">
+        <form onSubmit={handleSubmit}>
+          <h2> Settings </h2>
+          <div className="form-control w-full max-w">
+            <label className="label">
+              <span className="label-text">Search Engine</span>
+            </label>
+            <select
+              name="searchEngine"
+              className="select select-bordered w-full max-w"
+              defaultValue={settings.searchEngine}
+            >
+              <option value="google">Google</option>
+              <option value="bing">Bing</option>
+              <option value="duckduckgo">DuckDuckGo</option>
+            </select>
+          </div>
+
+          <div className="form-control w-full max-w mt-4">
+            <label className="label cursor-pointer">
+              <span className="label-text">Show Search Bar</span>
+              <input
+                type="checkbox"
+                name="searchBar"
+                className="toggle toggle-primary ml-2"
+                defaultChecked={settings.searchBar}
+              />
+            </label>
+          </div>
+          <button type="submit" className="btn btn-primary mt-4 w-full">
+            Save
+          </button>
+        </form>
+      </div>
+      <form method="dialog" className="modal-backdrop overflow-hidden">
+        <button>close</button>
+      </form>
+    </dialog>
+  );
+}
