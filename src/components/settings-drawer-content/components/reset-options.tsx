a7f316b346fb386fa5db84d0d4535ce39a0aa4ce
Kavinthe Perera <kavintheperera@gmail.com>
2024-07-10 04:00:26 +0530

    confirm modal for reset settings

--- src/components/settings-drawer-content/components/reset-options.tsx
+++ src/components/settings-drawer-content/components/reset-options.tsx
@@ -1,3 +1,5 @@
+// NOTE: Might want to replace with a antd modal or a custom modal
+//
 interface ResetOptionSettingsProps {
   resetSettings: () => void;
 }
@@ -6,11 +8,36 @@ export default function ResetOptionSettings({
 }: ResetOptionSettingsProps) {
   return (
     <>
+      <dialog id="reset_confirm_modal" className="modal">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">Reset Settings</h3>
+          <p className="py-4">
+            No Icons will be removed, but all settings will be reset to default.
+            Are you sure you want to reset?
+          </p>
+          <div className="modal-action">
+            <form method="dialog">
+              <button
+                className="btn btn-error flex-grow mr-2"
+                onClick={resetSettings}
+              >
+                Reset
+              </button>
+              <button className="btn">Cancel</button>
+            </form>
+          </div>
+        </div>
+      </dialog>
       <div className="divider text-sm">Reset</div>
       <div className="flex w-full max-w ">
         <button
           type="button"
-          onClick={resetSettings}
+          onClick={() => {
+            const modal = document.getElementById(
+              "reset_confirm_modal",
+            ) as HTMLDialogElement;
+            modal.showModal();
+          }}
           className="btn btn-outline btn-error flex-grow mr-2"
         >
           Reset Settings
