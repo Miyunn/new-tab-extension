68e1041a53acedc49133a6cfff113a489cd3f1c8
Kavinthe Perera <kavintheperera@gmail.com>
2024-08-04 08:48:30 +0530

    feat: remove user data
    nuke database from settings menu

--- src/components/settings-drawer-content/components/reset-options.tsx
+++ src/components/settings-drawer-content/components/reset-options.tsx
@@ -1,8 +1,29 @@
 // NOTE: Might want to replace with a antd modal or a custom modal
-//
+
+import IDBExportImport from "indexeddb-export-import";
+import db from "../../../database/indexDb";
+
 interface ResetOptionSettingsProps {
   resetSettings: () => void;
 }
+
+function nukeDatabase() {
+  db.open()
+    .then(() => {
+      const idbDatabase = db.backendDB();
+      IDBExportImport.clearDatabase(idbDatabase, (err: Error | null) => {
+        if (err) {
+          console.error("Clear database failed: ", err);
+        } else {
+          console.log("Database cleared successfully");
+        }
+      });
+    })
+    .catch((err) => {
+      console.error("Database open failed: ", err);
+    });
+}
+
 export default function ResetOptionSettings({
   resetSettings,
 }: ResetOptionSettingsProps) {
@@ -28,7 +49,28 @@ export default function ResetOptionSettings({
           </div>
         </div>
       </dialog>
-      <div className="divider text-sm">Reset</div>
+
+      <dialog id="removeData_confirm_modal" className="modal">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">Delete Data</h3>
+          <p className="py-4">
+            Your settings will be kept, but the icons and wallpaper will be
+            removed Are you sure you want to delete the data?
+          </p>
+          <div className="modal-action">
+            <form method="dialog">
+              <button
+                className="btn btn-error flex-grow mr-2"
+                onClick={nukeDatabase}
+              >
+                Reset
+              </button>
+              <button className="btn">Cancel</button>
+            </form>
+          </div>
+        </div>
+      </dialog>
+      <div className="divider text-sm">Delete</div>
       <div className="flex w-full max-w ">
         <button
           type="button"
@@ -45,9 +87,14 @@ export default function ResetOptionSettings({
         <button
           type="button"
           className="btn btn-outline btn-error w-1/2  ml-2"
-          disabled
+          onClick={() => {
+            const modal = document.getElementById(
+              "removeData_confirm_modal",
+            ) as HTMLDialogElement;
+            modal.showModal();
+          }}
         >
-          Remove Icons
+          Remove Data
         </button>
       </div>
     </>
