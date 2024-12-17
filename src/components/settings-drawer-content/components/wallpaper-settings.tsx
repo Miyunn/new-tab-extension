537287050c624c20b6069852a066a91b7915dbe4
Kavinthe Perera <kavintheperera@gmail.com>
2024-12-17 22:44:15 +0530

    feat: adds info tool tips for unsplash settings

--- src/components/settings-drawer-content/components/wallpaper-settings.tsx
+++ src/components/settings-drawer-content/components/wallpaper-settings.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { ColorPicker } from "antd";
+import { Tooltip } from "react-tooltip";
 
 interface WallpaperSettingsProps {
   settings: any;
@@ -150,12 +151,44 @@ export default function WallpaperSettings({
       {backgroundType === "unsplash" && (
         <>
           <div className="form-control w-full max-w py-2">
+            <Tooltip id="unsplash-quality-tool-tip" className="z-50">
+              Adjust the resolution of the Unsplash wallpaper to suit your
+              needs:
+              <br />- <strong>Low</strong>: Optimized for faster loading; ideal
+              when using heavy blur effects.
+              <br />- <strong>Medium</strong>: A balanced choice for good image
+              quality and quick load times.
+              <br />- <strong>High</strong>: Perfect for high-resolution
+              screens; may slow down the new tab's opening speed.
+              <br />- <strong>Original</strong>: Maximum resolution for the best
+              quality but may significantly impact loading performance.
+            </Tooltip>
+            <Tooltip id="unsplash-query-tool-tip" className="z-50">
+              Use this field to search Unsplash for wallpapers based on a
+              specific query.
+              <br />
+              For example, enter "nature" or "space" to find relevant images.
+              <br />
+              Results will be randomly selected based on your query.
+            </Tooltip>
             <label className="label">
               <span className="label-text">Wallpaper Query</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-question-circle"
+                viewBox="0 0 16 16"
+                data-tooltip-id="unsplash-query-tool-tip"
+              >
+                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                <path d="M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286m1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94" />
+              </svg>
             </label>
             <input
               type="text"
-              placeholder="Query unsplash for wallpapers"
+              placeholder="Type a keyword"
               className="input w-full max-w"
               name="unsplashQuery"
               defaultValue={settings.unsplashQuery}
@@ -165,6 +198,19 @@ export default function WallpaperSettings({
           <div className="form-control w-full max-w py-2">
             <label className="label">
               <span className="label-text">Wallpaper Quality</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-question-circle"
+                viewBox="0 0 16 16"
+                data-tip="Hello"
+                data-tooltip-id="unsplash-quality-tool-tip"
+              >
+                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                <path d="M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286m1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94" />
+              </svg>
             </label>
             <input
               type="range"
