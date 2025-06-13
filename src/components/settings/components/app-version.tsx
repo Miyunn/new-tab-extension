d8216da2d9d3b031bd44a14153b10a9f11da718e
Miyunn <kavintheperera@gmail.com>
2025-06-14 05:00:23 +0530

    chore: updates app version to 0.7.4

--- src/components/settings/components/app-version.tsx
+++ src/components/settings/components/app-version.tsx
@@ -1,7 +1,7 @@
 export default function AppVersion() {
   return (
     <div className="text-right text-xs text-slate-600 mt-3 pt-2">
-      App Version: 0.7.3
+      App Version: 0.7.4
     </div>
   );
 }
