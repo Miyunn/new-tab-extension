142e99900e5a854b5f9f959832a21953f12e06d1
Miyunn <kavintheperera@gmail.com>
2026-02-12 01:45:47 +0530

    chore(version): updates app versoin to 0.8.1

--- src/components/settings/components/app-version.tsx
+++ src/components/settings/components/app-version.tsx
@@ -1,7 +1,7 @@
 export default function AppVersion() {
   return (
     <div className="text-right text-xs text-slate-600 mt-3 pt-2">
-      App Version: 0.7.5
+      Version: 0.8.1
     </div>
   );
 }
